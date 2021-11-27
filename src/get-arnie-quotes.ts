dcfb1d9fa6e71b7f30e1f0e531193dc8a24a98cc
Gentle Joseph <gentle.joseph@cashrewards.com>
2021-11-27 21:40:55 +1100

    Arnie quote - test result

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,35 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+type KeyType = 'Arnie Quote' | 'FAILURE';
+type TResult = {
+  [key in KeyType]?: string;
+};
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+/**
+ * ? The goal is to write an implementation of getArnieQuotes() that meets all requirements and passes all unit tests.
+ * * If the HTTP status code of the response is 200, push an object to the results array
+ * * with a single key "Arnie Quote" and the HTTP response body's message property as the key's associated value.
+ *
+ *  * If the HTTP status code of the response is not 200,
+ *  * push an object to the results array with a single key "FAILURE"
+ *  * and the HTTP response body's message property as the key's associated value.
+ *
+ *  * Finally, the getArnieQuotes() function's return value must be a promise that resolves to the overall results array.
+ *
+ * @param urls
+ * @returns {Promise<TResult>}
+ */
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const results = await Promise.all(urls.map((url) => httpGet(url)));
+  return results.map(({ status, body }) => {
+    if (status === 200) {
+      return {
+        'Arnie Quote': JSON.parse(body)?.message,
+      };
+    }
+    return {
+      FAILURE: JSON.parse(body)?.message,
+    };
+  });
 };
